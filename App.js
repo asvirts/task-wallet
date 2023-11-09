9ae4c56c9c66fd4ca5cef8979a47ce840c2e9e9e
Andrew Virts <asvirts@outlook.com>
2023-11-08 21:28:32 -0600

    so cool

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is what my app looks like!</Text>
+      <Text>More text</Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#eee",
     alignItems: "center",
     justifyContent: "center",
   },
