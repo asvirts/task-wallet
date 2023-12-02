d859cb1f8a5ad2c5eb8dfa3256451b666c8ef705
Andrew Virts <asvirts@outlook.com>
2023-12-02 14:45:17 -0600

    defining user type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import GetAllTasks from "@/components/GetAllTasks";
-import type { Metadata } from "next";
+import GetAllTasks from "@/app/components/GetAllTasks"
+import type { Metadata } from "next"
+import { SessionProvider } from "next-auth/react"
+import type { AppProps } from "next/app"
 
 export const metadata: Metadata = {
   title: "TaskWallet",
   description: "Your tasks, with you wherever you go",
-};
+}
 
-export default function Home() {
+export default function Home({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <main className="p-5">
       <h1 className="font-bold text-4xl">TaskWallet</h1>
-      <GetAllTasks />
+      <SessionProvider session={session}>
+        <GetAllTasks {...pageProps} />
+      </SessionProvider>
     </main>
-  );
+  )
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,12 @@
 export type Task = {
   id: number
   name: string
-  due_date: Date
+  due_date: date
+  created_on: date
+}
+
+export type User = {
+  id: number
+  name: string
+  created_on: date
 }
