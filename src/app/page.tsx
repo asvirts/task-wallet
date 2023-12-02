b4b3812be027554473ff8e21da818ebedd7e9463
Andrew Virts <asvirts@outlook.com>
2023-12-02 15:04:20 -0600

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
 import GetAllTasks from "@/app/components/GetAllTasks"
 import type { Metadata } from "next"
-import { SessionProvider } from "next-auth/react"
-import type { AppProps } from "next/app"
 
 export const metadata: Metadata = {
   title: "TaskWallet",
   description: "Your tasks, with you wherever you go",
 }
 
-export default function Home({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+export default function Home() {
   return (
     <main className="p-5">
       <h1 className="font-bold text-4xl">TaskWallet</h1>
-      <SessionProvider session={session}>
-        <GetAllTasks {...pageProps} />
-      </SessionProvider>
+      <GetAllTasks />
     </main>
   )
 }
