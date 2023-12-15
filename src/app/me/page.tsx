e23234e573d049a54b11ae5d150796f99b046169
Andrew Virts <asvirts@outlook.com>
2023-12-15 15:10:26 -0600

    delete

--- src/app/me/page.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth"
-
-export default async function Me() {
-  const session = await auth()
-
-  const userId = 1
-
-  return <div>{JSON.stringify(session, null, 2)}</div>
-}
